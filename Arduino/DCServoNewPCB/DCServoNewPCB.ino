f14e3f307de27085ff7e98a3493f5fafab296734
Seb Lee-Delisle <sebleedelisle@gmail.com>
2014-11-25 20:37:45 +0000

    Added PWM frequency change
    Signed-off-by: Seb Lee-Delisle <sebleedelisle@gmail.com>

--- Arduino/DCServoNewPCB/DCServoNewPCB.ino
+++ Arduino/DCServoNewPCB/DCServoNewPCB.ino
@@ -57,6 +57,15 @@ PID myPID(&position, &motorPower, &targetPositionDouble, Kp, Ki, Kd, DIRECT);
 
 void setup() {
 
+  // set PWM frequency to 31250
+  setPwmFrequency(9, 1);
+  setPwmFrequency(10, 1);
+  
+  // set PWM frequency to 3906
+//  setPwmFrequency(9, 8);
+//  setPwmFrequency(10, 8);
+ 
+  
   command.reserve(256);
 
   readEepromData(); 
@@ -283,5 +292,40 @@ ISR(PCINT2_vect)
       targetPositionLong-=stepMultiplier; 
   }
 }
+
+
+
+//
+void setPwmFrequency(int pin, int divisor) {
+  byte mode;
+  if(pin == 5 || pin == 6 || pin == 9 || pin == 10) {
+    switch(divisor) {
+      case 1: mode = 0x01; break;
+      case 8: mode = 0x02; break;
+      case 64: mode = 0x03; break;
+      case 256: mode = 0x04; break;
+      case 1024: mode = 0x05; break;
+      default: return;
+    }
+    if(pin == 5 || pin == 6) {
+      TCCR0B = TCCR0B & 0b11111000 | mode;
+    } else {
+      TCCR1B = TCCR1B & 0b11111000 | mode;
+    }
+  } else if(pin == 3 || pin == 11) {
+    switch(divisor) {
+      case 1: mode = 0x01; break;
+      case 8: mode = 0x02; break;
+      case 32: mode = 0x03; break;
+      case 64: mode = 0x04; break;
+      case 128: mode = 0x05; break;
+      case 256: mode = 0x06; break;
+      case 1024: mode = 0x7; break;
+      default: return;
+    }
+    TCCR2B = TCCR2B & 0b11111000 | mode;
+  }
+}
+
 
 
